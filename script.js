d94cf894b6a7bb9fb1d68881d1ee163133305b65
egjavier <yunisjavier002@gmail.com>
2023-09-23 23:17:39 +0300

    added comments and styles

--- script.js
+++ script.js
@@ -3,15 +3,35 @@ const todoInput = document.querySelector('#todoInput');
 const taskList = document.querySelector('#taskList');
 
 addTask.addEventListener('click', () => {
-  const li = document.createElement('li');
-  const newTask = document.createTextNode(`${todoInput.value}`);
-  li.appendChild(newTask);
-  taskList.appendChild(li);
+  if (todoInput.value === '') {
+    // if input field is emplty, there will be an alert
+    alert('You must enter a task first!');
+  } else {
+    // created <li> element under <ul>
+    const li = document.createElement('li');
+    const newTask = document.createTextNode(`${todoInput.value}`);
+    li.appendChild(newTask);
+    taskList.appendChild(li);
 
-  li.addEventListener('click', () => {
-    li.classList.toggle('done');
-  })
+    // whenever <li> or added task is clicked, a line through the task will appear
+    li.addEventListener('click', () => {
+      li.classList.toggle('done');
+    })
 
+    // added <button> x
+    const xBtn = document.createElement('button');
+    const x = document.createTextNode('x');
+    xBtn.appendChild(x);
+    li.appendChild(xBtn);
+
+    // when x button is clicked, <li> will be removed from <ul>
+    xBtn.addEventListener('click', () => {
+      taskList.removeChild(li);
+    })
+  }
+
+  // after adding task, input field will be empty
+  todoInput.value = '';
 })
 
 
